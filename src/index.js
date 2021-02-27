da59e0fd47d032d7e516de53bc73329df5ec858c
Vlad0703 <73859766+Vlad0703@users.noreply.github.com>
2021-02-28 00:01:54 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ module.exports = function toReadable (n) {
     19: 'nineteen',
     20: 'twenty',
     30: 'thirty',
-    40: 'fourty',
+    40: 'forty',
     50: 'fifty',
     60: 'sixty',
     70: 'seventy',
