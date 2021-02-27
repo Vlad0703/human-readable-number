812f64751e1178e1f4dc800d630cc2ee30a4d14c
Vlad0703 <73859766+Vlad0703@users.noreply.github.com>
2021-02-27 23:46:51 +0300

    Update index.js
    init: star human readable number task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,57 @@
 module.exports = function toReadable (number) {
+    let read = {
+    
+    1: 'one',
+    2: 'two',
+    3: 'three',
+    4: 'four',
+    5: 'five',
+    6: 'six',
+    7: 'seven',
+    8: 'eight',
+    9: 'nine',
+    10: 'ten',
+    11: 'eleven',
+    12: 'twelve',
+    13: 'thirteen',
+    14: 'fourteen',
+    15: 'fifteen',
+    16: 'sixteen',
+    17: 'seventeen',
+    18: 'eighteen',
+    19: 'nineteen',
+    20: 'twenty',
+    30: 'thirty',
+    40: 'fourty',
+    50: 'fifty',
+    60: 'sixty',
+    70: 'seventy',
+    80: 'eighty',
+    90: 'ninety',
+  }
+  let hundreds = '';
+  let tenners = '';
+  let ones = '';
+  let num = String(n);
+
+  if (num.length === 3){
+    hundreds = String(read[num.split('')[0]] + ' hundred')
+  }
   
+  if (num.length >= 2) {
+    if (num.split('').slice(-2).join('') < 20) {
+      tenners = read[num.split('').slice(-2).join('')]
+      
+    } else {
+      tenners = read[num.split('').slice(-2).join('') - num.split('').slice(-1)];
+      ones = read[num.split('').slice(-1)];
+      if (num.split('').slice(-1) == 0) {
+        ones = '';
+      }
+    }
+
+  } else if (num.length = 1) {
+    num == 0 ? ones = 'zero' : ones = read[num];
+  }
+    return (hundreds + ' ' + tenners +' '+ones)
 }
